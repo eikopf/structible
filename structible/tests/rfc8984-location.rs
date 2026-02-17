873ca1cc5907e630e73be7546584dcd6f00ca9e7
Oliver Wooding <oliverwooding@icloud.com>
2026-02-17 10:55:58 +0100

    small update to the rfc8984-location test

--- structible/tests/rfc8984-location.rs
+++ structible/tests/rfc8984-location.rs
@@ -7,15 +7,36 @@ type Id = str;
 type VendorStr = str;
 
 /// A rough version of the Location type from RFC 8984 §4.2.5.
-#[structible]
+#[structible(with_len)]
 struct Location<V> {
     pub name: String,
     pub description: Option<String>,
-    pub location_types: HashSet<String>,
+    pub location_types: Option<HashSet<String>>,
     pub relative_to: Option<String>,
     pub time_zone: Option<String>,
     pub coordinates: Option<String>,
-    pub links: HashMap<Box<Id>, Link>,
+    pub links: Option<HashMap<Box<Id>, Link>>,
     #[structible(key = Box<VendorStr>)]
     pub vendor_property: Option<V>,
+}
+
+#[test]
+fn basic_usage() {
+    let mut location = Location::<bool>::new("Sydney".into());
+    location.set_links(Some(HashMap::new()));
+    location.add_vendor_property("example.com:foo".into(), true);
+    location.add_vendor_property("example.com:bar".into(), false);
+
+    assert_eq!(location.len(), 4);
+    assert_eq!(location.vendor_property("example.com:foo"), Some(&true));
+    assert_eq!(location.vendor_property("example.com:bar"), Some(&false));
+
+    if let Some(links) = location.links_mut() {
+        links.insert("baz".into(), ());
+        links.insert("qux".into(), ());
+    }
+
+    assert_eq!(location.name(), "Sydney");
+    location.name_mut().push_str(" Harbor Bridge");
+    assert_eq!(location.name(), "Sydney Harbor Bridge");
 }
